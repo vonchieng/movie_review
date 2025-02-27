1f01e5e5cd6746bcc950b32ad5e64405c95a46e6
Eyvon Chieng <166025169+vonchieng@users.noreply.github.com>
2025-02-27 14:54:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from joblib import load
 import os
 
 # Download NLTK resources if not already downloaded
-nltk.download('punkt')
+nltk.download('punkt_tab')
 
 # Load the saved Naive Bayes classifier
 bayes_classifier = load('bayes_classifier.joblib')
